d1b0122849571e2bf3e7a09ef9873be8f6173ab0
Adrian Hrinko <adriqueh@gmail.com>
2025-05-24 12:15:08 +0200

    added args to userdbcontextfactory

--- backend/Services/TaskService/TaskService.Infrastructure/Database/TaskDbContextFactory.cs
+++ backend/Services/TaskService/TaskService.Infrastructure/Database/TaskDbContextFactory.cs
@@ -6,8 +6,17 @@ public class TaskDbContextFactory : IDesignTimeDbContextFactory<TaskDbContext>
 {
     public TaskDbContext CreateDbContext(string[] args)
     {
+        var connectionString = args.Length > 0 
+            ? args[0] 
+            : null;
+
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new InvalidOperationException("Connection string not found. Please provide it either through args");
+        }
+
         var optionsBuilder = new DbContextOptionsBuilder<TaskDbContext>();
-        optionsBuilder.UseSqlServer("Server=localhost,1433;Database=TaskDb;User Id=sa;Password=YourStrong!Passw0rd;TrustServerCertificate=True;");
+        optionsBuilder.UseSqlServer(connectionString); // "Server=localhost,1433;Database=TaskDb;User Id=sa;Password=YourStrong!Passw0rd;TrustServerCertificate=True;";
 
         return new TaskDbContext(optionsBuilder.Options);
     }
--- backend/Services/UserService/UserService.Infrastructure/Database/UserDbContextFactory.cs
+++ backend/Services/UserService/UserService.Infrastructure/Database/UserDbContextFactory.cs
@@ -6,8 +6,17 @@ public class UserDbContextFactory : IDesignTimeDbContextFactory<UserDbContext>
 {
     public UserDbContext CreateDbContext(string[] args)
     {
+        var connectionString = args.Length > 0 
+            ? args[0] 
+            : null;
+
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new InvalidOperationException("Connection string not found. Please provide it either through args ");
+        }
+            
         var optionsBuilder = new DbContextOptionsBuilder<UserDbContext>();
-        optionsBuilder.UseSqlServer("Server=localhost,1433;Database=UserDb;User Id=sa;Password=YourStrong!Passw0rd;TrustServerCertificate=True;");
+        optionsBuilder.UseSqlServer(connectionString); // "Server=localhost,1433;Database=UserDb;User Id=sa;Password=YourStrong!Passw0rd;TrustServerCertificate=True;"
 
         return new UserDbContext(optionsBuilder.Options);
     }
